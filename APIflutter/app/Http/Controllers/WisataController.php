f07df3a355477ca16a1137d2d9fdfae21cc7aa50
galihr <galih.rashif@gmail.com>
2022-01-19 20:01:20 +0700

    controller

--- APIflutter/app/Http/Controllers/WisataController.php
+++ APIflutter/app/Http/Controllers/WisataController.php
@@ -2,9 +2,128 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wisata;
 use Illuminate\Http\Request;
 
 class WisataController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+       return Wisata::all();
+    }
+
+    public function indexs()
+    {
+        //
+        $tampils = Kas::orderBy('id')->get();
+        return view('kas',compact('tampils'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(request $request)
+    {
+        //
+        $wisata = new Wisata;
+        $kas->jumlah = $request->jumlah;
+        $kas->deskripsi = $request->deskripsi;
+        $kas->divisi = $request->divisi;
+        $wisata->save();
+
+        return "data berhasil masuk";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kas  $kas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kas $kas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kas  $kas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $edit = Kas::findorFail($id);
+        return view('kasedit', compact('edit'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kas  $kas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $jumlah = $request->jumlah;
+        $deskripsi = $request->deskripsi;
+        $divisi = $request->divisi;
+
+        $kas = Kas::find($id);
+        $kas->jumlah = $jumlah;
+        $kas->deskripsi = $deskripsi;
+        $kas->divisi = $divisi;
+        $kas->save();
+        
+        return redirect()->route('kas.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kas  $kas
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        //
+        $kas = Kas::find($id);
+        $kas->delete();
+        return redirect()->route('kas.index');
+    }
+
+    public function kas()
+    {
+        //
+        $kas = Kas::all();
+
+        return view("/kas", ["kas" => $kas]);
+    }
+
+    public function add(){
+        return view ('kasadd');
+    }
+
 }
--- APIflutter/routes/api.php
+++ APIflutter/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WisataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('wisata','WisataController@index');
+Route::post('wisata','WisataiController@create');
+Route::put('/wisata/{id}','WisataController@update');
+Route::delete('/wisata/{id}','WisataController@delete');
