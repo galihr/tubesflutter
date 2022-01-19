afb0ab88d9648160454610953f2110eb84a32d50
galihr <galih.rashif@gmail.com>
2022-01-19 21:48:07 +0700

    model wisata

---
+++ flutterapi/app/Models/Wisata.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wisata extends Model
+{
+    protected $table = 'wisata';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pelanggan', 'harga','desc','tanggal',
+    ];
+
+} 
